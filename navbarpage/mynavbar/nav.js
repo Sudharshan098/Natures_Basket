622e20a0c07a6c6f364df5e4ded4121217ffb18e
Aman Patel <amanpatel639250@gmail.com>
2022-06-18 11:15:31 +0530

    navbarpage

---
+++ navbarpage/mynavbar/nav.js
@@ -0,0 +1,38 @@
+function navbar(){
+    return`<div id="imgbox">
+    <img src="https://www.naturesbasket.co.in/Images/logosnew.png?v=2" alt="">
+    </div>
+    <div id="navbarbox"> 
+       <div id="input">
+        <div class="hii"><a href="">Online Slots Availability  |</a></div>
+        <div class="hii"><a href="">Fresh & Fast |</a></div>
+        <div class="hii"> <a href="">Store Locator |</a></div>
+        <div class="hii"> <a href="">Contact Us |</a></div>
+    <select  id="naya" class="hii">
+        <option value="mumbai">Mumbai</option>
+        <option value="ooty">Pune</option>
+        <option value="china">Delhi NCR</option>
+        <option value=" chandigarg">Bengaluru</option>
+        <option value="nepal">Thane</option>
+        <option value="nepl">Navi Mumbai</option>
+        <option value="ne">Kolkata</option>
+        <option value="nepa">Raigarh MH</option>
+    
+    </select>
+        <div class="hii"><a href="">Enter Pincode</a></div>
+       </div>
+       <div id="input1">
+        <input id="bottle" type="search" placeholder=" Start shopping..."/>
+        <div id="gayab2"><img id="tellme" src="https://www.naturesbasket.co.in/Images/search-button.jpg"/></div>
+    </div>
+    
+    </div>
+    <div id="register">
+       <div class="hh2"><a href="">Login</a></div>
+       <div class="hh2"><a href="">Register</a></div>
+    </div>
+    `
+    
+    }
+    
+    export default navbar;
